ef09633d1fd42c4c3aae5bbc46b3d44b5fe73feb
leonardo <quevedoruiz41@gmail.com>
2024-12-22 18:18:31 -0500

    Add port as a global variable

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './common/envs';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const PORT = envs.port ?? 3002;
+  const logger = new Logger('tasks-ms');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
+
+  logger.log(`Tasks microservices running on port ${PORT}`);
 }
 bootstrap();
